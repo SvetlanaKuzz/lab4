022b73044e37723a95127dfea6c5165e1f742d3c
SvetlanaKuzz <svetlana112owo@gmail.com>
2024-03-17 21:36:27 +0300

    1

---
+++ laba4.py
@@ -0,0 +1,51 @@
+def z1():
+    a = int(input("Введите число: "))
+    if a % 3 == 0:
+        print(f"Число {a} делится на 3")
+    else:
+        print(f"Число {a} не делится на 3")
+
+def z2():
+ try:
+     a = int(input("Введите число: "))
+     res = 100 / a
+     print(f"Результат деления 100 на {a} : {res}")
+ except ValueError:
+     print("Ошибка! Вы ввели не число")
+ except ZeroDivisionError:
+     print("Ошибка! Деление на ноль")
+
+def z3():
+    a = int(input("Введите день: "))
+    b = int(input("Введите месяц: "))
+    c = input("Введите год: ")
+
+    d = int(c[-2:])
+    if a * b == d:
+        print("Дата магическая!")
+    else:
+        print("Дата НЕ магическая")
+
+def z4():
+    f = 0
+    s = 0
+    a = str(input("Введите номер билета: "))
+    if len(a) != 6:
+        print("Неверный номер")
+    else:
+        half = len(a) // 2
+        f_half = int(a[:half])
+        s_half = int(a[half:])
+        for i in range(half):
+            f = int(f) + int(a[i])
+        for i in range(half, len(a)):
+            s = int(s) + int(a[i])
+        if f == s:
+            print(f"Билет {a} - счастливый")
+        else:
+            print(f"Билет {a} - несчастливый")
+
+z1()
+z2()
+z3()
+z4()
